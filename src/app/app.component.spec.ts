7a5fca40174611ccc9dab146fc2bc2597d0f5f9f
Pankaj Raut <pankaj.raut.mca@gmail.com>
2020-07-10 18:46:35 +0530

    feat(VersionChanged): Using Angular 9.1.3

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,11 +2,39 @@ import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
+
+
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { DialogModule } from 'primeng/dialog';
+
+
+import {InputTextModule} from 'primeng/inputtext';
+import {CheckboxModule} from 'primeng/checkbox';
+import {RadioButtonModule} from 'primeng/radiobutton';
+import {InputTextareaModule} from 'primeng/inputtextarea';
+import {TabViewModule} from 'primeng/tabview';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+import { AccordionModule, MenuItem } from 'primeng/primeng';
+
+import { AppRoutingModule } from './app-routing.module';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        ReactiveFormsModule,
+        FormsModule,
+
+        AppRoutingModule,
+        InputTextModule,
+        CheckboxModule,
+        ButtonModule,
+        RadioButtonModule,
+        InputTextareaModule,
+        CardModule,
+        ButtonModule,
+        DialogModule
       ],
       declarations: [
         AppComponent
@@ -20,16 +48,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Task-Management-Application'`, () => {
+  it(`should have as title 'Task Management Application'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Task-Management-Application');
+    expect(app.title).toEqual('Task Management Application');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Task-Management-Application app is running!');
+    expect(compiled.querySelector('.toolbar span').textContent).toContain('Task Management Application');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { DialogModule } from 'primeng/dialog';
+
+
+import {InputTextModule} from 'primeng/inputtext';
+import {CheckboxModule} from 'primeng/checkbox';
+import {RadioButtonModule} from 'primeng/radiobutton';
+import {InputTextareaModule} from 'primeng/inputtextarea';
+import {TabViewModule} from 'primeng/tabview';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MessagesModule} from 'primeng/messages';
+import {MessageModule} from 'primeng/message';
+
+import { AccordionModule, MenuItem } from 'primeng/primeng';
+
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -10,9 +28,25 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ReactiveFormsModule,
+    FormsModule,
+
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    InputTextModule,
+    CheckboxModule,
+    ButtonModule,
+    RadioButtonModule,
+    InputTextareaModule,
+    CardModule,
+    ButtonModule,
+    DialogModule,
+    MessagesModule,
+    MessageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
+
 })
+
 export class AppModule { }
